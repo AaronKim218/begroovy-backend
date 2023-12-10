894aca2233ea07d2b0893da26501500129fd32e4
aaronkimbrooktec <218adk@gmail.com>
2023-12-10 17:28:57 -0500

    add port env var

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.
 console.log(DB_CONNECTION_STRING)
 mongoose.connect(DB_CONNECTION_STRING);
 
-const PORT = 3000
-
-app.listen(PORT, () => console.log(`Local server is listening on port ${PORT}`));
+app.listen(process.env.PORT || 4000);
